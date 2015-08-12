2901c3d46605afbfd652082db2d3e50e0f4f60b8
alfonsopintos <alfonsopintos@gmail.com>
2015-08-11 20:48:52 -0400

    places search table view working

---
+++ testing_1/PlacesTableViewController.m
@@ -0,0 +1,88 @@
+//
+//  PlacesTableViewController.m
+//  testing_1
+//
+//  Created by Alfonso Pintos on 8/11/15.
+//  Copyright © 2015 Meme Menu. All rights reserved.
+//
+
+#import "PlacesTableViewController.h"
+#import "PlacesTableViewCell.h"
+
+@interface PlacesTableViewController () <UISearchBarDelegate, UISearchDisplayDelegate>
+
+@property (nonatomic, strong) NSArray *allData;
+@property (nonatomic, strong) NSArray *searchResults;
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+
+@end
+
+@implementation PlacesTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.allData = @[@"One", @"Two", @"Three", @"Four", @"Five",
+                     @"Six", @"Seven", @"Eight", @"Nine", @"Ten"];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section
+    if (tableView == self.tableView) {
+        return self.allData.count;
+    } else {
+        return self.searchResults.count;
+    }
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+    }
+    
+    
+    if (tableView == self.tableView) {
+        cell.textLabel.text = [self.allData objectAtIndex:indexPath.row];
+    } else {
+        cell.textLabel.text = [self.searchResults objectAtIndex:indexPath.row];
+    }
+    
+    
+        
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString *title = [tableView cellForRowAtIndexPath:indexPath].textLabel.text;
+    self.title = title;
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+#pragma mark - Search Methods
+
+-(void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+        self.searchResults = nil;
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF contains [cd] %@", self.searchBar.text];
+        
+    self.searchResults = [self.allData filteredArrayUsingPredicate:predicate];
+}
+
+
+
+
+@end
