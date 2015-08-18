c41398a4636b19befd4a1c03c2b1cf589529b01b
alfonsopintos <alfonsopintos@gmail.com>
2015-08-18 17:21:17 -0400

    added reskit and FileMD5Hash pods

---
+++ Pods/Target Support Files/LibComponentLogging-Core/LibComponentLogging-Core-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LibComponentLogging_Core : NSObject
+@end
+@implementation PodsDummy_LibComponentLogging_Core
+@end
---
+++ Pods/Target Support Files/LibComponentLogging-NSLog/LibComponentLogging-NSLog-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LibComponentLogging_NSLog : NSObject
+@end
+@implementation PodsDummy_LibComponentLogging_NSLog
+@end
