6a83ea4a09ab9f41ab331873a82b09febae18572
alfonsopintos <alfonsopintos@gmail.com>
2015-08-23 14:31:55 -0400

    removed lists from sidebar nav

--- testing_1/SidebarTableViewController.m
+++ testing_1/SidebarTableViewController.m
@@ -24,7 +24,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.revealViewController.delegate = self;
-    menu = @[@"Home", @"Around You", @"Feed", @"Lists", @"Your List"];
+    menu = @[@"Home", @"Around You", @"Feed", @"Your List"];
     
     self.tableView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"sidebar-background"]];
 }
