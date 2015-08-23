91e7cc75825f1e9989834ced46087a8b81301df8
alfonsopintos <alfonsopintos@gmail.com>
2015-08-23 14:22:27 -0400

    renamed nearby to around you

--- testing_1/SidebarTableViewController.m
+++ testing_1/SidebarTableViewController.m
@@ -24,7 +24,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.revealViewController.delegate = self;
-    menu = @[@"Home", @"Nearby", @"Feed", @"Lists", @"Places", @"Your List"];
+    menu = @[@"Home", @"Around You", @"Feed", @"Lists", @"Places", @"Your List"];
     
     self.tableView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"sidebar-background"]];
 }
