a6aebeff4d9f4b3dfae1cf864e9cac620c590d2a
alfonsopintos <alfonsopintos@gmail.com>
2015-08-24 11:58:02 -0400

    set featured collection view to scrolling and not paging

--- testing_1/SidebarTableViewController.m
+++ testing_1/SidebarTableViewController.m
@@ -93,7 +93,7 @@
     UIView *selectedView = [[UIView alloc] init];
     selectedView.backgroundColor = [UIColor clearColor];
     cell.selectedBackgroundView = selectedView;
-    cell.textLabel.highlightedTextColor = [UIColor darkGrayColor];
+    cell.textLabel.highlightedTextColor = [UIColor colorWithRed:(30/255) green:(139/255) blue:(195/255) alpha:1];
     return cell;
 }
 
